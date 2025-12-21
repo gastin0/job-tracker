4e57d8e9c4793be60121a4c3883c1260e56fce2a
gastino <andre.abdirrosyid@gmail.com>
2025-12-21 23:45:17 +0700

    Implement job app CRUD API

---
+++ src/app/api/applications/route.js
@@ -0,0 +1,13 @@
+const ALLOWED_STATUS = [
+    "Applied",
+    "Test",
+    "Interview",
+    "offer",
+    "Rejected"
+]
+
+const ALLOWED_WORK_TYPE = [
+    "Remote",
+    "Hybrid",
+    "On-site"
+]
