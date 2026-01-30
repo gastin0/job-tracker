5ca5b59a06aa7e0588fd2c2e2dc4c266233fc46b
gastino <andre.abdirrosyid@gmail.com>
2026-01-30 16:55:56 +0700

    Disable correct rule name

--- src/components/ConfirmDeleteModal.jsx
+++ src/components/ConfirmDeleteModal.jsx
@@ -20,7 +20,7 @@ export default function ConfirmDeleteModal({
 
     useEffect(() => {
         setHasMounted(true);
-        // eslint-disable-next-line react-hooks/set-state-in-effect
+        // eslint-disable-next-line
     }, []);
 
     useEffect(() => {
